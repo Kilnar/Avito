beb487c0e8fabe6ee7befb9db17c2c3fdb207b70
Alexey Stratonov <a.stratonov@simcase.ru>
2015-06-30 22:44:56 +0300

    Работает заполдение карточками.

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -5,16 +5,30 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
+import java.util.ArrayList;
+
 public class MainController {
 
     @FXML
-    ListView<String> ads;
+    private ListView<String> listView;
+    private ObservableList observableList;
+    private ArrayList<String> stringArrayList;
 
+    public MainController(){
+        observableList = FXCollections.observableArrayList();
+        stringArrayList = new ArrayList<String>();
+    }
     @FXML
     private void initialize() {
-        ObservableList<String> names = FXCollections.observableArrayList(
-                "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
-        ads.setItems(names);
+        setListView();
+    }
+    public void setListView(){
+        stringArrayList.add("String 1");
+        stringArrayList.add("String 2");
+        stringArrayList.add("String 3");
+        observableList.setAll(stringArrayList);
+        listView.setItems(observableList);
+        listView.setCellFactory(param -> new ListViewCell());
     }
 
 }
---
+++ src/sample/data/Data.java
@@ -0,0 +1,6 @@
+package sample.data;
+
+/**
+ * Created by strat on 30.06.15.
+ */
+

e8a8f628a7bdbd75b78b62d750ee870c505c8faa
Alexey Stratonov <a.stratonov@simcase.ru>
2015-06-30 20:12:53 +0300

    простое заполнение работает

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,5 +1,20 @@
 package sample.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class MainController {
+
+    @FXML
+    ListView<String> ads;
+
+    @FXML
+    private void initialize() {
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+        ads.setItems(names);
+    }
 
 }
